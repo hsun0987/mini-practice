5004674374d89b89cf976bdf9e6a4f074f4ba6b1
hsun0987 <hsun0987@daum.net>
2025-07-26 15:02:01 +0900

    init project

---
+++ src/main/java/com/example/mini_practice/MiniPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPracticeApplication.class, args);
+	}
+
+}
